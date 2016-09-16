3e8971ecdd40de675e466645549cfdb8225bf2a6
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2016-09-16 11:58:52 -0700

    add functionality to render ebook automatically

--- render.sh
+++ render.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 
-Rscript -e 'bookdown::render_book("index.Rmd")'
+if [ $# -lt 1]; then
+	echo
+	echo "Rendering EBOOK"
+	echo
+	echo "If you want to render the PDF, set the -pdf flag"
+	echo
+	echo "    ./render.sh -pdf"
+	echo
+	sed -i 's/^[ \t]*\(beaver.*pdf.*$\)/  # \1/' index.Rmd
+	sed -i 's/^[ \t]*# \(beaver.*gitbook.*$\)/   \1/' index.Rmd
+	Rscript -e 'bookdown::render_book("index.Rmd")'
+	sed -i 's/^[ \t]*# \(beaver.*pdf.*$\)/   \1/' index.Rmd
+	sed -i 's/^[ \t]*\(beaver.*gitbook.*$\)/  # \1/' index.Rmd
+else
+	echo
+	echo "Rendering PDF"
+	echo
+	Rscript -e 'bookdown::render_book("index.Rmd")'
+fi
+
