368368bf97d951f732298b39d559f034c1288bc7
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2016-09-16 12:12:44 -0700

    render works now

--- render.sh
+++ render.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
-if [ $# ]; then
-	echo
+if [[ $# -eq 0 ]]; then
+	echo "----------------------------------------------------------------------"
 	echo "Rendering EBOOK"
 	echo
 	echo "If you want to render the PDF, set the -pdf flag"
 	echo
 	echo "    ./render.sh -pdf"
+	echo "----------------------------------------------------------------------"
 	echo
+	# Temporarily changing pdf for gitbook
 	sed -i '' 's/^[ \t]*\(beaver.*pdf.*$\)/  # \1/' index.Rmd
 	sed -i '' 's/^[ \t]*# \(beaver.*gitbook.*$\)/  \1/' index.Rmd
+	# Rendering the book
 	Rscript -e 'bookdown::render_book("index.Rmd")'
+	# Changing things back to the way they were
 	sed -i '' 's/^[ \t]*# \(beaver.*pdf.*$\)/  \1/' index.Rmd
 	sed -i '' 's/^[ \t]*\(beaver.*gitbook.*$\)/  # \1/' index.Rmd
 else
